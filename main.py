9a5d1aae05e095928237329c885277dcdd5b0fe7
pwdkramer <patrick.kramer07@gmail.com>
2022-01-04 17:43:13 -0800

    added difficulty selection function

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 #CodeCademy portfolio project 1: Mastermind
 #1/4/22
 
+def chooseDifficulty():
+    print("Please select a difficulty: (n/h)")
+    user_difficulty = input()
+    while user_difficulty.lower() != "n" and user_difficulty.lower() != "h":
+        print("Please enter either 'n' or 'h'")
+        user_difficulty = input()
+    return user_difficulty.lower()
+
+
 print("Welcome to Mastermind!")
 
 #select difficulty
-print("Please select a difficulty: (n/h)")
-user_difficulty = input()
-while user_difficulty.lower() != "n" and user_difficulty.lower() != "h":
-  print("Please enter either 'n' or 'h'")
-  user_difficulty = input()
+diff = chooseDifficulty()
